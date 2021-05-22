28b7e880e446aa326b328f4ee2cd6a3c3e0ad8f6
huang-melissa <mhuang.teaching@gmail.com>
2021-05-22 00:06:42 -0400

    edits

--- tree.js
+++ tree.js
@@ -1,165 +1,148 @@
-/* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.70,
-  height = window.innerHeight * 0.70,
-  margin = { top: 20, bottom: 60, left: 60, right: 60 },
-  radius = 5;
+/**
+ * CONSTANTS AND GLOBALS
+ * */
+const width = window.innerWidth * 0.9,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
+/** these variables allow us to access anything we manipulate in
+ * init() but need access to in draw().
+ * All these variables are empty before we assign something to them.*/
 let svg;
-let xScale;
-let yScale;
 
-/* APPLICATION STATE */
+/**
+ * APPLICATION STATE
+ * */
 let state = {
-  data: [],
-  selectedParty: "All"
+  // + SET UP STATE
+  geojson: null,
+  Temperature: null,
+  hover: {
+    screenPosition: null, // will be array of [x,y] once mouse is hovered on something
+    mapLat: null, // value of long 
+    mapLong: null, // value of lat
+    stateName : null,
+    tempChange: null,
+    visible: false,
+  }
 };
 
-/* LOAD DATA */
-d3.csv("../data/WHR20.csv", d3.autoType).then(raw_data => {
-  console.log("data", raw_data);
-  state.data = raw_data;
+/**
+ * LOAD DATA
+ * Using a Promise.all([]), we can load more than one dataset at a time
+ * */
+Promise.all([
+  d3.json("usState.json"),
+  d3.csv("geo_data.csv", d3.autoType),
+]).then(([geojson, Temperature]) => {
+  // + SET STATE WITH DATA
+  state.geojson = geojson
+  state.Temperature = Temperature
+  console.log("state: ", state);
   init();
 });
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
+/**
+ * INITIALIZING FUNCTION
+ * this will be run *one time* when the data finishes loading in
+ * */
 function init() {
-  console.log('State:', state)
-  //SCALES
-    xScale = d3.scaleLinear()
-      .domain(d3.extent(state.data, d => d.healthy_life))
-      .range([margin.left, width - margin.right]) 
+  const projection = d3.geoAlbersUsa() //it will goes to long/lat => x/y
+  .fitSize([width, height], state.geojson)
 
-    yScale = d3.scaleLinear()
-      .domain(d3.extent(state.data, d => d.ladder_score))
-      .range([height - margin.bottom, margin.top]) //our min value is at the bottom, max value is at the top of our svg 
 
-    colorScale = d3.scaleLinear()
-      .domain(d3.extent(state.data, d => d.ladder_score))
-      .range(["#2a8d8c", "#f5b845"])
+  const colorScaleL = d3.scaleLinear([0, d3.max(state.Temperature)])
 
-  //AXES
-    const xAxis = d3.axisBottom(xScale)
-    const yAxis = d3.axisLeft(yScale)
+  const pathFunction = d3.geoPath(projection)
 
-  //Creating SVG
-    svg = d3.select("#d3-container")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
+// create an svg element in our main 'd3-container' element 
+  svg = d3
+    .select("#geo-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  //Adding Axes
-    svg.append("g")
-      .attr("class", "xAxis")
-      .attr("transform", `translate(${0}, ${height - margin.bottom})`)
-      .call(xAxis)
-      .append("text")
-      .attr("class", 'axis-title')
-      .attr("x", width / 2)
-      .attr("y", 40)
-      .attr("font-size", 10)
-      .attr("font-weight", "bold")
-      .attr("letter-spacing", "0.2em")
-      .attr("style", "fill: #ffffff")
-      .attr("text-anchor", "middle")
-      .text("Healthy Life Expectancy")
 
-    svg.append("g")
-      .attr("class", "yAxis")
-      .attr("transform", `translate(${margin.left}, ${0})`)
-      .call(yAxis)
-      .append("text")
-      .attr("class", 'axis-title')
-      .attr("x", -40)
-      .attr("y", height / 2)
-      .attr("font-size", 10)
-      .attr("font-weight", "bold")
-      .attr("letter-spacing", "0.2em")
-      .attr("style", "fill: #ffffff; writing-mode: tb; glyph-orientation-vertical: 0")
-      .attr("text-anchor", "middle")
-      .text("Happiness Score")
+// base layer of states
+const states = svg.selectAll("path")
+.data(state.geojson.features)
+.join("path")
+.attr("stroke", "white")
+.attr("fill", "#7e9776")
+.attr("d", pathFunction)
 
-    //SETUP  UI ELEMENTS
-    const dropdown = d3.select("#dropdown") 
-      
-    dropdown.selectAll("options")
-      .data([
-      {key: "All", label: "All"}, 
-      {key: "Central and Eastern Europe", label: "Central & Eastern Europe"}, 
-      {key: "Commonwealth of Independent States", label: "Commonwealth of Independent States"},
-      {key: "East Asia", label: "East Asia"},
-      {key: "Latin America and Caribbean", label: "Latin America & Caribbean"},
-      {key: "Middle East and North Africa", label: "Middle East & North Africa"},
-      {key: "North America and ANZ", label: "North America & ANZ"},
-      {key: "South Asia", label: "South Asia"},
-      {key: "Southeast Asia", label: "Southeast Asia"},
-      {key: "Sub-Saharan Africa", label: "Sub-Saharan Africa"},
-      {key: "Western Europe", label: "Western Europe"}])
-      .join("option")
-      .attr("value", d => d.key)
-      .text(d => d.label)
-    
-    dropdown.on("change", event => {
-      console.log("selection changed", event.target.value) 
-      state.selectedParty = event.target.value
-      console.log("updated state", state)
-      draw();
-    })
+// create circles
 
-      draw();
+svg.selectAll("circle")
+.data(state.Temperature)
+.join("circle")
+.attr("stroke", "white")
+.attr("fill", d=>{
+  if (d.Change_in_95_percent_Days>0) return "#cc6a3f" // tangerine
+  else if (d.Change_in_95_percent_Days===0) return "#dad5aa" // beige 
+  else return "#6ecbee" // frozen blue
+})
+// could not get Math.abs to work, so created new column of absolute values
+.attr("r", d=>{ 
+  if (d.Change_in_95_percent_Days_abs>30) return 15;
+  else if (d.Change_in_95_percent_Days_abs>1 || d.Change_in_95_percent_Days_abs<29) return 7;
+  else if (d.Change_in_95_percent_Days_abs===0 || d.Change_in_95_percent_Days_abs<9) return 2;
+  else return 1
+})
+.attr("fill-opacity", "0.5")
+.attr("transform", d=>{
+  console.log(d)
+  const point = projection([d.Long, d.Lat])
+  return `translate(${point[0]}, ${point[1]})`
+} )
 
+states.on("mouseover", function(event, d){
+  const {clientX, clientY} = event
+
+  const [long, lat] = projection.invert([clientX, clientY])
+
+  state.hover= {
+    stateName: [d.State],
+    tempChange: [d.Change_in_95_percent_Days_abs],
+    screenPosition: [clientX, clientY],
+    mapLong: [d3.format(".4f")(long)],
+    mapLat: [d3.format(".4f")(lat)],
+    visible: true,
+  }
+
+  draw();
+
+}).on("mouseout", event => {
+  state.hover.visible = true
+  draw(); // calls the draw function
+})
+
+draw();
 }
 
-/* DRAW FUNCTION */
-// we call this everytime there is an update to the data/state
+/**
+ * DRAW FUNCTION
+ * we call this everytime there is an update to the data/state
+ * */
 function draw() {
 
-  // + FILTER DATA BASED ON STATE
-  const filteredData = state.data
-  .filter(d => {
-    if (state.selectedParty === "All") return true 
-    else return d.regional_indicator === state.selectedParty
-  })
-
-  svg.selectAll("circle")
-    .data(filteredData, d => d.country_name) //arbitrary ID# to make each data point unique   
-    .join(
-        enter => enter.append("circle")
-          .attr("r", radius)
-          .attr("fill", d => colorScale(d.ladder_score))
-          .style("stroke-opacity", .50)
-          .style("stroke", "#ffffff")
-          .attr("cy", margin.top)
-          .attr("cx", d => xScale(d.healthy_life))
-          .call(enter => enter
-            .transition()
-            .ease(d3.easeCircleIn)
-            .duration(1000)
-            .attr("cy", d => yScale(d.ladder_score))
-          ),
-
-        update => update
-            .call(sel => sel
-              .transition()
-              .duration(250)
-              .attr("r", radius * 1.5)
-              .transition()
-              .duration(500)
-              .attr("r", radius)
-           ),
-           
-        exit => exit
-          .attr("cy", d => yScale(d.ladder_score))
-          .attr("cx", d => xScale(d.healthy_life))
-            .call(exit => exit
-              .transition()
-              .style("opacity", .25)
-              .duration(1000)
-              .attr("cx", width - margin.right)
-              .attr("cy", height / 2)
-              .remove()
-          )
-      );
+d3.select("#geo-container") // want to add
+    .selectAll('div.hover-content')
+    .data([state.hover])
+    .join("div")
+    .attr("class", 'hover-content')
+    .classed("visible", d=> d.visible)
+    .style("position", 'absolute')
+    .style("transform", d=> {
+      // only move if we have a value for screenPosition
+      if (d.screenPosition)
+      return `translate(${d.screenPosition[0]}px, ${d.screenPosition[1]}px)`
+    })
+    .html(d=> {
+      return `
+      <div> State: ${d.stateName}</div>
+      <div> Temperature Change: ${d.tempChange}<div>
+      <div> Longitude: ${d.mapLong}</div>
+      <div> Latitude: ${d.mapLat}`}
+      )
 }
